bc9cddaeb554c4ed27432a59e71782d067c1c10d
jos-b2 <diegocenteno288@gmail.com>
2025-11-05 15:24:47 -0600

    vista servicios

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,9 @@ def index():
     """Ruta principal de la aplicacion"""
     return render_template('layouts/index.html', title='Inicio')
 
-@bp.route('/Servicios')
+@bp.route('/servicios')
 def servicios():
     """Ruta de servicios"""
-    return render_template('Servicios.html', title='Servicios')
+    return render_template('Modules/Gestion_Servicio/Vista2.html', title='Servicios')
+
 
