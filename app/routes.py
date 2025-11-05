28ec065243b3fa3c5c2843d7ffd337102d20fd6b
Magmacraft89 <Emerson409denis@gmail.com>
2025-11-05 16:04:01 -0600

    cambios en routes y base.html

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template # type: ignore
 
 bp = Blueprint('main', __name__)
 
@@ -8,10 +8,17 @@ def index():
     """Ruta principal de la aplicacion"""
     return render_template('layouts/index.html', title='Inicio')
 
+<<<<<<< HEAD
 @bp.route('/servicios')
 def servicios():
     """Ruta de servicios"""
     return render_template('Modules/Gestion_Servicio/Vista2.html', title='Servicios')
+=======
+@bp.route('/ubicaciones')
+def ubicacion():
+    """Ruta de servicios de ubicaciones"""
+    return render_template('Modules/Gestion_Ubicaciones/Vista4.html', title='Servicios')
+>>>>>>> f6c97e6 (cambios en routes y base.html)
 
 
 <<<<<<< HEAD
