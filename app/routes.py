de13b8ff003351c6f4c6f06e6a6bd96654881aa1
jos-b2 <diegocenteno288@gmail.com>
2025-11-09 15:21:59 -0600

    Diseño preliminar de la vista2 (servicios) y creacion de un archivo css para darle un toque de color a algunas secciones

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,13 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/')
+
 @bp.route('/index')
 def index():
     """Ruta principal de la aplicacion"""
     return render_template('layouts/index.html', title='Inicio')
 
+@bp.route('/servicios')
 def servicios():
     """Ruta de servicios"""
     return render_template('Modules/Gestion_Servicio/Vista2.html', title='Servicios')
