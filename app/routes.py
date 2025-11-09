5a697a1ce8106dc0b91e0dd357ecd5021d7e9e0a
Josereph <josephorell05@gmail.com>
2025-11-09 17:08:24 -0600

    Cambios sobre diseño en mi rama

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
-
+@bp.route('/')
 @bp.route('/index')
 def index():
     """Ruta principal de la aplicacion"""
@@ -14,8 +14,14 @@ def servicios():
     return render_template('Modules/Gestion_Servicio/Vista2.html', title='Servicios')
 
 
-# 🔹 NUEVA RUTA → Mini menú de Recursos Operativos
 @bp.route('/recursos')
 def recursos():
     """Mini menú de recursos operativos"""
     return render_template('layouts/MiniMenuRecursos.html', title='Recursos Operativos')
+
+
+@bp.route('/vehiculos')
+def vehiculos():
+    """Vista del módulo de gestión de vehículos"""
+    return render_template('Modules/Gestion_Vehiculos/VistaGestionVehiculos.html', title='Gestión de Vehículos')
+
