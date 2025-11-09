14f98075cb3b210594736a0e7b9736366fff8f3e
jos-b2 <diegocenteno288@gmail.com>
2025-11-09 15:21:59 -0600

    Diseño preliminar de la vista2 (servicios) y creacion de un archivo css para darle un toque de color a algunas secciones

--- app/routes.py
+++ app/routes.py
@@ -2,38 +2,12 @@ from flask import Blueprint, render_template # type: ignore
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/')
+
 @bp.route('/index')
 def index():
     """Ruta principal de la aplicacion"""
     return render_template('layouts/index.html', title='Inicio')
 
-
-
-@bp.route('/gestion_evidencia') 
-def gestion_evidencia():
-    """Módulo de Gestión de Evidencias y Documentación"""
-    servicios = [
-        {'Id_Servicio': 1, 'cliente_nombre': 'Agropecuaria Los Pinos'},
-        {'Id_Servicio': 2, 'cliente_nombre': 'Distribuidora San José'},
-        {'Id_Servicio': 3, 'cliente_nombre': 'Cooperativa El Progreso'}
-    ]
-    return render_template('modules/Gestion_Evidencia/Vista.html', title='Gestión de Evidencia',servicios=servicios)
-
-
-
-
-@bp.route('/ubicaciones')
-def ubicaciones():
-    """Ruta de servicios de ubicaciones"""
-    return render_template('Modules/Gestion_Ubicaciones/Vista4.html', title='Ubicaciones')
-
-@bp.route('/detalles')
-def detalles():
-    """Ruta de servicios de detalles"""
-    return render_template('Modules/Gestion_Ubicaciones/detalles.html', title='detalles')
-
-# Alias en minúsculas para evitar confusiones con /Servicios
 @bp.route('/servicios')
 def servicios():
     """Ruta de servicios"""
