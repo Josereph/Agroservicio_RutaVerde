aa9fa99fcd0af77b9e1d51f88041125e764f5652
Arthur072910 <pocasangrerene729gmail.com>
2025-11-09 19:48:30 -0600

    Correcion de errores

--- app/routes.py
+++ app/routes.py
@@ -26,16 +26,34 @@ def ubicaciones():
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+
+@bp.route('/conductores')
+def conductores():
+    """Ruta de conductores"""
+    return render_template("Modules/Gestion_Conductores/chepe.html", title='Conductores')
+
+
+# 🔹 NUEVA RUTA → Mini menú de Recursos Operativos
+>>>>>>> 321e235 (Correcion de errores)
 @bp.route('/recursos')
 def recursos():
     """Mini menú de recursos operativos"""
     return render_template('layouts/MiniMenuRecursos.html', title='Recursos Operativos')
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 321e235 (Correcion de errores)
 
 @bp.route('/vehiculos')
 def vehiculos():
     """Vista del módulo de gestión de vehículos"""
     return render_template('Modules/Gestion_Vehiculos/VistaGestionVehiculos.html', title='Gestión de Vehículos')
+<<<<<<< HEAD
 
 >>>>>>> 5a697a1 (Cambios sobre diseño en mi rama)
+=======
+>>>>>>> 321e235 (Correcion de errores)
