9f152245e67da7e4dfff6c6c45c0cb379d0effaf
jos-b2 <diegocenteno288@gmail.com>
2025-11-10 14:16:15 -0600

    nuevos cambios para el merge de documentacion

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,19 @@ def index():
     """Ruta principal de la aplicacion"""
     return render_template('layouts/index.html', title='Inicio')
 
+
+
+@bp.route('/gestion_evidencia') 
+def gestion_evidencia():
+    """Módulo de Gestión de Evidencias y Documentación"""
+    servicios = [
+        {'Id_Servicio': 1, 'cliente_nombre': 'Agropecuaria Los Pinos'},
+        {'Id_Servicio': 2, 'cliente_nombre': 'Distribuidora San José'},
+        {'Id_Servicio': 3, 'cliente_nombre': 'Cooperativa El Progreso'}
+    ]
+    return render_template('modules/Gestion_Evidencia/Vista.html', title='Gestión de Evidencia',servicios=servicios)
+
+
 @bp.route('/ubicaciones')
 def ubicaciones():
     """Ruta de servicios de ubicaciones"""
@@ -23,8 +36,6 @@ def detalles():
 def servicios():
     """Ruta de servicios"""
     return render_template('Modules/Gestion_Servicio/Vista2.html', title='Servicios')
-
-
 
 @bp.route('/conductores')
 def conductores():
