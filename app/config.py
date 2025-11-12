3df06edd90c19c4e11efea8f64ba638cf050592e
Arthur072910 <pocasangrerene729gmail.com>
2025-11-12 14:57:32 -0600

    conexion a bdd

--- app/config.py
+++ app/config.py
@@ -1,12 +1,19 @@
 import os
 
+
 class Config:
     """Configuración de la aplicación"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'tu-clave-secreta-aqui-cambiar-en-produccion'
     
     # Configuración de base de datos (ejemplo con SQLite)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RAHP2907/RutaVerdeBD'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True #para ver las consultas SQL en la consola
     
     # Otras configuraciones
-    DEBUG = True
+    class DevelopmentConfig(Config):
+        DEBUG = True
+    
+    class ProductionConfig(Config):
+        DEBUG = False
+    
